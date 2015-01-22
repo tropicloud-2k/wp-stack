09680ed6e8d6f9d0bac759e701970ebc804459ac
tropicloud <admin@tropicloud.net>
2015-01-22 17:13:10 -0200

    Thu Jan 22 17:13:10 BRST 2015

--- wps/func/wps-start.sh
+++ wps/func/wps-start.sh
@@ -2,8 +2,8 @@
 
 env > /etc/env
 
-if [[  ! -f '/app/wp-config.php'  ]];   then wps setup; fi
-if [[  ! -f '/var/log/nginx.log'  ]];   then touch /var/log/nginx.log; fi
+if [[  ! -f '/app/wp-config.php'  ]]; then wps setup; fi
+if [[  ! -f '/var/log/nginx.log'  ]]; then touch /var/log/nginx.log; fi
 if [[  ! -f '/var/log/php-fpm.log'  ]]; then touch /var/log/php-fpm.log; fi
 if [[  ! -f '/tmp/supervisor.sock'  ]]; then touch /tmp/supervisor.sock; fi
 
