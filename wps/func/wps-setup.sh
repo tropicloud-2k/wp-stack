aa1e493db4e2df4e08da689e2f67682fdefcde6e
tropicloud <admin@tropicloud.net>
2015-01-22 06:34:54 -0200

    Thu Jan 22 06:34:54 BRST 2015

--- wps/func/wps-setup.sh
+++ wps/func/wps-setup.sh
@@ -79,8 +79,8 @@ function wps_setup() {
 	wp --allow-root core download	
 	cat /usr/local/wps/conf/wordpress/wp-config.php > /app/wp-config.php
 
-	chown -R nginx:nginx /app/wordpress
-	chown -R nginx:nginx /app/wp-config.php
-	chmod -R 755 /app/wordpress
-	chmod -R 755 /app/wp-config.php
+# 	chown -R nginx:nginx /app/wordpress
+# 	chown -R nginx:nginx /app/wp-config.php
+# 	chmod -R 755 /app/wordpress
+# 	chmod -R 755 /app/wp-config.php
 }
