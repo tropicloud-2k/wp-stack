24d883689775d1514c908755ac7dd46dcb826a49
tropicloud <admin@tropicloud.net>
2015-01-22 05:53:51 -0200

    Thu Jan 22 05:53:51 BRST 2015

--- wps/func/wps-start.sh
+++ wps/func/wps-start.sh
@@ -2,5 +2,6 @@
 
 if [[  ! -f '/var/log/nginx.log'  ]]; then touch /var/log/nginx.log; fi
 if [[  ! -f '/var/log/php-fpm.log'  ]]; then touch /var/log/php-fpm.log; fi
+if [[  ! -f '/tmp/supervisor.sock'  ]]; then touch /tmp/supervisor.sock; fi
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
