127dadb0e1d082aebeaafe5cf1fd5aaeaf17d8d0
tropicloud <admin@tropicloud.net>
2015-01-22 16:55:12 -0200

    Thu Jan 22 16:55:12 BRST 2015

--- wps/func/wps-setup.sh
+++ wps/func/wps-setup.sh
@@ -4,10 +4,12 @@
 
 function wps_setup() {
 	
+	
 	mkdir -p /app/wordpress
 	cd /app/wordpress
 	
 	wp --allow-root core download	
+	cat /etc/env | grep 'DATABASE_URL'.* | cut -d= -f2 > /app/env/database_url
 	cat /usr/local/wps/conf/wordpress/wp-config.php > /app/wp-config.php
 	cat /usr/local/wps/conf/wordpress/db.php > /app/wordpress/db.php
 
