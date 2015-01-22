4b25b87d12f33a687ca760b623e5dd97bb116cb3
tropicloud <admin@tropicloud.net>
2015-01-22 04:21:12 -0200

    Thu Jan 22 04:21:12 BRST 2015

--- wps/wps-env.sh
+++ wps/wps-env.sh
@@ -2,13 +2,8 @@
 # ENV
 # ------------------------
 
-# DB_HOST=$(env | grep -o 'MARIADB_PORT_3306_TCP_ADDR'.* | cut -d= -f2)
-# DB_PORT=$(env | grep -o 'MARIADB_PORT_3306_TCP_PORT'.* | cut -d= -f2)
-
-DB_HOST=$(echo $DATABASE_URL | grep -o @.* | cut -d: -f1 | sed 's/@//g')
-DB_PORT=$(echo $DATABASE_URL | grep -o @.* | cut -d: -f2 | cut -d/ -f1 )
-
-DB_NAME=$(env | grep -o 'DATABASE_URL'.* | cut -d@ -f2 | cut -d\/ -f2)
-DB_USER=$(env | grep -o 'DATABASE_URL'.* | cut -d: -f2 | sed 's|//||g')
-DB_PASS=$(env | grep -o 'DATABASE_URL'.* | cut -d: -f3 | cut -d@ -f1)
-
+DB_HOST=$( echo $DATABASE_URL | grep -o @.* | cut -d: -f1 | cut -d@ -f2 )
+DB_PORT=$( echo $DATABASE_URL | grep -o @.* | cut -d: -f2 | cut -d/ -f1 )
+DB_NAME=$( echo $DATABASE_URL | grep -o @.* | cut -d/ -f2 )
+DB_USER=$( echo $DATABASE_URL | cut -d: -f2 | sed 's|//||g' )
+DB_PASS=$( echo $DATABASE_URL | cut -d: -f3 | cut -d@ -f1 )
