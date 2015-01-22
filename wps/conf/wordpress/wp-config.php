4c05e03fd985760666fef888867fdaece32c9ef0
tropicloud <admin@tropicloud.net>
2015-01-22 14:19:30 -0200

    Thu Jan 22 14:19:30 BRST 2015

--- wps/conf/wordpress/wp-config.php
+++ wps/conf/wordpress/wp-config.php
@@ -1,17 +1,6 @@
 <?php
 
-// ** Dokku MySQL settings - from Dokku Environment ** //
-$db = parse_url($_ENV["DATABASE_URL"]);
-
-define('DB_NAME', trim($db["path"],"/"));
-define('DB_USER', $db["user"]);
-define('DB_PASSWORD', $db["pass"]);
-//define('DB_HOST', $db["host"].":".$db["port"]);  // <---- add .":".$db["port"]
-define('DB_HOST', getenv('MARIADB_PORT_3306_TCP_ADDR').":".getenv('MARIADB_PORT_3306_TCP_PORT'));
-
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-
+// ** WP Keys ** //
 define('AUTH_KEY',         '&<CF0(WcE%2mF7/o(_kT9mA{++ %P-k&=][UuEZ^~>/O<sl>.TM0>sL]>cuduaBe');
 define('SECURE_AUTH_KEY',  '/sD43~)mf/!SlKzbpnZA,R/r|mb/ep}^%&SlqS3#JEo-//+8)}4eOr[pPP4wPQrx');
 define('LOGGED_IN_KEY',    'Y--;AdM];6k`i}92+*|hW_F5YG|#S|Ta|3C,N9&@j@DsA>a{:y=(_0_B?0n{OQbw');
@@ -20,14 +9,27 @@ define('AUTH_SALT',        'R(vmIz`-li2QKZ_gdYDJ@Kj $e#-x9=oSB)c:r[N8cVrx|w~ZOAw
 define('SECURE_AUTH_SALT', 'I(c#a2+%#c1t~KT]CgWTD$X^YEOP9S@*V O]is$8R^QF~8Kc|!a!|Ngj!Ftx#b$r');
 define('LOGGED_IN_SALT',   'mcb3 s46r&)r@uBMXjLS#:*BMO[WZr8I|w+}2{%~uR|uw|veuijkq.8zaoCfj#j_');
 define('NONCE_SALT',       '^LKVxF%v.P1O8Ru{TCNx>_2~=(tTeu3pn`&;_Yk@jG-WI{_0K;H :!Oh}!PV z1z');
-define('WPCACHEHOME',      '/app/wordpress/wp-content/plugins/wp-super-cache/'               );
 
+// ** MariaDB ** //
+$db = parse_url($_ENV["DATABASE_URL"]);
+define('DB_NAME', trim($db["path"],"/"));
+define('DB_USER', $db["user"]);
+define('DB_PASSWORD', $db["pass"]);
+define('DB_HOST', $db["host"].":".$db["port"]);  // <---- add .":".$db["port"]
+//define('DB_HOST', getenv('MARIADB_PORT_3306_TCP_ADDR').":".getenv('MARIADB_PORT_3306_TCP_PORT'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 $table_prefix = 'wp_';
 
-define('DISALLOW_FILE_EDIT', true);
-//define('FORCE_SSL_LOGIN', true);
-//define('FORCE_SSL_ADMIN', true);
+// ** WP Super Cache ** //
+define('WPCACHEHOME',      '/app/wordpress/wp-content/plugins/wp-super-cache/'               );
 define('WP_CACHE', true);
+
+
+// ** WP SSL ** //
+define('FORCE_SSL_LOGIN', true);
+define('FORCE_SSL_ADMIN', true);
+define('DISALLOW_FILE_EDIT', true);
 
 
 /* That's all, stop editing! Happy blogging. */
