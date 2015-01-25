8486357e3dc10c2b0f52d34cbcce7da4ae73cdce
tropicloud <admin@tropicloud.net>
2015-01-25 15:17:26 -0200

    Sun Jan 25 15:17:25 BRST 2015

--- conf/wordpress/db.php
+++ conf/wordpress/db.php
@@ -3,6 +3,8 @@
 $database_url = parse_url(exec('cat /etc/environment | grep DATABASE_URL | cut -d= -f2'));
 
 print_r($database_url);
-echo $database_url;
+
+$db_name = trim($database_url['path'],'/'));
+echo $db_name;
 
 ?>
