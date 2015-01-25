8ae3c21ebfd8fa9084746bd015ae44f173affbac
tropicloud <admin@tropicloud.net>
2015-01-24 23:38:43 -0200

    Sat Jan 24 23:38:43 BRST 2015

--- _func/wps-start.sh
+++ _func/wps-start.sh
@@ -1,14 +1,12 @@
 # ------------------------
-# nps START
+# WPS START
 # ------------------------
 
-function nps_start() {
-
-	cd $nps
+function wps_start() {
 
 	if [[  ! -f '/var/log/php-fpm.log'  ]]; then touch /var/log/php-fpm.log; fi
 	if [[  ! -f '/var/log/nginx.log'    ]]; then touch /var/log/nginx.log; fi
-	if [[  ! -f '/app/wp-config.php'    ]]; then nps_wp_install; fi
+	if [[  ! -f '/app/wp-config.php'    ]]; then wps_wp_install; fi
 	
 	/usr/bin/supervisord -n -c /etc/supervisord.conf
 	
