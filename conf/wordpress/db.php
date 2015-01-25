cba4e9af9332abfbe4fb14b8032fc932b7a53519
tropicloud <admin@tropicloud.net>
2015-01-25 15:11:32 -0200

    Sun Jan 25 15:11:32 BRST 2015

--- conf/wordpress/db.php
+++ conf/wordpress/db.php
@@ -2,14 +2,7 @@
 
 $database_url = parse_url(exec('cat /etc/environment | grep DATABASE_URL | cut -d= -f2'));
 
-$db_name = trim($database_url['path'],'/'));
-$db_user = $database_url['user']);
-$db_pass = $database_url['pass']);
-$db_host = $database_url['host'].':'.$database_url['port']);
-
-echo "DB_NAME: $db_name";
-echo "DB_USER: $db_user";
-echo "DB_PASS: $db_pass";
-echo "DB_HOST: $db_host";
+print_r($database_url);
+echo $database_url;
 
 ?>
