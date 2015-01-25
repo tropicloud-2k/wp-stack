d9b97dcf3561bb1e2cb68bd48c5c816db53a8fc9
tropicloud <admin@tropicloud.net>
2015-01-25 18:41:00 -0200

    Sun Jan 25 18:41:00 BRST 2015

--- conf/wordpress/db.php
+++ conf/wordpress/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$database_url = parse_url($_ENV['cat /etc/environment | grep DATABASE_URL']);
+$database_url = parse_url($_ENV['cat /etc/environment | grep DATABASE_URL | cut -d= -f2']);
 
 print_r(parse_url($database_url));
 
