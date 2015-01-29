e9de0de36f671895b01960d6152c46d1a3211149
tropicloud <admin@tropicloud.net>
2015-01-29 17:43:22 -0200

    Thu Jan 29 17:43:22 BRST 2015

--- func/wps-setup.sh
+++ func/wps-setup.sh
@@ -56,16 +56,6 @@ EOF
 	wp --allow-root post meta update 2 header_image "//s3.tropicloud.net/wps-cli/img/slide-vantagens.jpg"
 
 	# ------------------------
-	# WELCOME EMAIL
-	# ------------------------
-	
-	ADM_MAIL='admin@tropicloud.net'
-	MANDRILL='JVjDagu4lFHJhgjjY2yfKw'
-	
-	echo -e "Sending welcome email..."
-	curl -s -A 'Mandrill-Curl/1.0' -d '{"key":"'$MANDRILL'","template_name":"wp-stack","template_content":[{"name":"title","content":"[WP-STACK] New WordPress Site"}],"message":{"subject":"[WP-STACK] New WordPress Site","from_email":"'$ADM_MAIL'","from_name":"Tropicloud","to":[{"email":"'$WP_MAIL'","name":"'$WP_USER'","type":"to"}],"headers":{"Reply-To":"'$ADM_MAIL'"},"important":true,"track_opens":true,"track_clicks":true,"auto_text":false,"auto_html":false,"inline_css":false,"url_strip_qs":false,"preserve_recipients":true,"view_content_link":false,"bcc_address":"'$ADM_MAIL'","tracking_domain":null,"signing_domain":null,"return_path_domain":null,"merge":true,"global_merge_vars":[{"name":"WP_URL","content":"'$WP_URL'"},{"name":"WP_USER","content":"'$WP_USER'"},{"name":"WP_PASS","content":"'$WP_PASS'"}]}}' 'https://mandrillapp.com/api/1.0/messages/send-template.json' | jq '.'
-
-	# ------------------------
 	# SSL CERT.
 	# ------------------------
 
@@ -84,4 +74,10 @@ EOF
 	chown nginx:nginx -R /app/wordpress && chmod 755 -R /app/wordpress
 	chown nginx:nginx /app/wp-config.php && chmod 750 -R /app/wp-config.php
 
+	# ------------------------
+	# WELCOME EMAIL
+	# ------------------------
+	
+	$wps/func/wps-mail welcome -d $WP_URL -u $WP_USER -p $WP_PASS -m $WP_MAIL | jq '.'
+	
 }
