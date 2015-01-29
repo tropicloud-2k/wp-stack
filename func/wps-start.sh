a419b06d13809305efd3d29ba14f6dc90e93a93c
tropicloud <admin@tropicloud.net>
2015-01-29 16:58:15 -0200

    Thu Jan 29 16:58:15 BRST 2015

--- func/wps-start.sh
+++ func/wps-start.sh
@@ -4,12 +4,23 @@
 
 function wps_start() {
 
-	wps_environment
+	if [[  -f /tmp/supervisord.pid  ]]; then
+		
+		if [[  -z $2  ]];
+		then /usr/bin/supervisorctl start all;
+		else /usr/bin/supervisorctl start $2;
+		fi
+		
+	else
+	
+		wps_environment
+	
+		if [[  ! -f '/var/log/php-fpm.log'  ]]; then touch /var/log/php-fpm.log; fi
+		if [[  ! -f '/var/log/nginx.log'    ]]; then touch /var/log/nginx.log; fi
+		if [[  ! -f '/app/wp-config.php'    ]]; then wps_setup; fi
+		
+		/usr/bin/supervisord -n -c /etc/supervisord.conf
+	
+	fi
 
-	if [[  ! -f '/var/log/php-fpm.log'  ]]; then touch /var/log/php-fpm.log; fi
-	if [[  ! -f '/var/log/nginx.log'    ]]; then touch /var/log/nginx.log; fi
-	if [[  ! -f '/app/wp-config.php'    ]]; then wps_setup; fi
-	
-	/usr/bin/supervisord -n -c /etc/supervisord.conf
-	
 }
