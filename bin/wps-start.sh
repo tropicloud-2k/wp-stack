b4e6d85d82bc66a88e86622c768860feb10836f0
tropicloud <admin@tropicloud.net>
2015-03-05 15:10:14 -0300

    Thu Mar  5 15:10:13 BRT 2015

--- bin/wps-start.sh
+++ bin/wps-start.sh
@@ -19,7 +19,7 @@ function wps_start() {
 		if [[  ! -f '/var/log/nginx.log'    ]]; then touch /var/log/nginx.log; fi
 		if [[  ! -f '/app/wp-config.php'    ]]; then wps_setup; fi
 		
-		/usr/bin/supervisord -n -c /etc/supervisord.conf
+		exec /usr/bin/supervisord -n -c /etc/supervisord.conf
 	
 	fi
 
