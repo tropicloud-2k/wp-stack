32b3fd2ff5e992ace2869a59fdbcf639c7e5d8c5
tropicloud <admin@tropicloud.net>
2015-03-05 22:05:40 -0300

    Thu Mar  5 22:05:40 BRT 2015

--- bin/wps-env.sh
+++ bin/wps-env.sh
@@ -6,21 +6,21 @@ function wps_environment() {
 	
 	if [[  -z $DATABASE_URL  ]]; then
 
-		DB_HOST=$(env | grep 'MARIADB_PORT_3306_TCP_ADDR' | cut -d= -f2)
-		DB_PORT=$(env | grep 'MARIADB_PORT_3306_TCP_PORT' | cut -d= -f2)
-		DB_NAME=$(env | grep 'MARIADB_ENV_MYSQL_DATABASE' | cut -d= -f2)
-		DB_USER=$(env | grep 'MARIADB_ENV_MYSQL_USER' | cut -d= -f2)
-		DB_PASS=$(env | grep 'MARIADB_ENV_MYSQL_PASSWORD' | cut -d= -f2)
-		DB_PROT=$(env | grep 'MARIADB_PORT_3306_TCP_PROTO' | cut -d= -f2)
+		DB_HOST=$(env | grep 'DB_PORT_3306_TCP_ADDR' | cut -d= -f2)
+		DB_PORT=$(env | grep 'DB_PORT_3306_TCP_PORT' | cut -d= -f2)
+		DB_NAME=$(env | grep 'DB_ENV_MYSQL_DATABASE' | cut -d= -f2)
+		DB_USER=$(env | grep 'DB_ENV_MYSQL_USER' | cut -d= -f2)
+		DB_PASS=$(env | grep 'DB_ENV_MYSQL_PASSWORD' | cut -d= -f2)
+		DB_PROT=$(env | grep 'DB_PORT_3306_TCP_PROTO' | cut -d= -f2)
 		
-		export DATABASE_URL=${DB_PROT}://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}
-
 		export DB_HOST=$DB_HOST
 		export DB_PORT=$DB_PORT
 		export DB_NAME=$DB_NAME
 		export DB_USER=$DB_USER
 		export DB_PASS=$DB_PASS
 		export DB_PROT=$DB_PROT
+
+		export DATABASE_URL=${DB_PROT}://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 
 	else
 
--- bin/wps-start.sh
+++ bin/wps-start.sh
@@ -16,10 +16,10 @@ function wps_start() {
 	else
 	
 		if [[  ! -f '/var/log/php-fpm.log'  ]]; then touch /var/log/php-fpm.log; fi
-		if [[  ! -f '/var/log/nginx.log'    ]]; then touch /var/log/nginx.log; fi
-		if [[  ! -f '/app/wp-config.php'    ]]; then wps_setup; fi
+		if [[  ! -f '/var/log/nginx.log'  ]]; then touch /var/log/nginx.log; fi
+		if [[  ! -f '/app/wp-config.php'  ]]; then wps_setup; fi
 		
-		exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+		exec "/usr/bin/supervisord -n -c /etc/supervisord.conf"
 	
 	fi
 
