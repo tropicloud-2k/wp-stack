e8238e2cc715438f74036d48dfcb4258a75338b6
tropicloud <admin@tropicloud.net>
2015-03-13 17:43:40 -0300

    Fri Mar 13 17:43:40 BRT 2015

--- bin/wps-install.sh
+++ bin/wps-install.sh
@@ -102,14 +102,15 @@ PHP
 	# ------------------------
 	
 	cat $wps/conf/ninjafirewall/htninja > $home/.htninja
-	wp --quiet --allow-root plugin install ninjafirewall --activate 
+	wp --allow-root plugin install ninjafirewall --activate 
 	
 	# ------------------------
 	# FIX PERMISSIONS
 	# ------------------------
 
-	chown wpstack:nginx -R $home && chmod 770 -R $home
+	chown wpstack:nginx -R $home && chmod 755 -R $home
 	chown wpstack:nginx -R $home/wp && chmod 775 -R $home/wp
+	chown wpstack:nginx $home/wp-config.php && chmod 770 $home/wp-config.php
 
 	# ------------------------
 	# WELCOME EMAIL
