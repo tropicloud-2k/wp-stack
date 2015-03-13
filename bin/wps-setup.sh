8a66a0edc9d80e17b9378f164dec6a90cf7540cc
tropicloud <admin@tropicloud.net>
2015-03-13 14:02:22 -0300

    Fri Mar 13 14:02:22 BRT 2015

--- bin/wps-setup.sh
+++ bin/wps-setup.sh
@@ -10,10 +10,9 @@ function wps_setup() {
 	# Chroot user
 	# ------------------------
 	
+	if [[  -d /app  ]]; then rm -rf /app; fi
 	useradd -g nginx -d $home -s /bin/false wpstack
-	chown root:root $home && chmod 750 $home
 	
-	mkdir -p $home/log
 	mkdir -p $home/ssl
 	mkdir -p $home/wp
 	
@@ -28,13 +27,16 @@ function wps_setup() {
 	cat $wps/conf/nginx/block.conf				> /etc/nginx/block.conf
 	cat $wps/conf/nginx/drop.conf				> /etc/nginx/drop.conf
 	cat $wps/conf/nginx/errorpages.conf			> /etc/nginx/errorpages.conf
- 	cat $wps/conf/nginx/wp.conf 				> /app/app.conf
+ 	cat $wps/conf/nginx/wp.conf 				> /etc/nginx/conf.d/default.conf
 
 	# ------------------------
 	# Permissions
 	# ------------------------
 
-	chown -R wpstack:nginx $home/wp  && chmod 750 -R $home/wp
-	chown -R wpstack:nginx $home/log && chmod 750 -R $home/log
+	chown -R root:root $home
+	chmod 750 $home
+	
+	chown -R wpstack:nginx $home/wp
+	chmod 750 -R $home/wp
 	
 }
